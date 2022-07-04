3f147eb61a392aec0bcaed7433d27ce30e528db2
wrenfannin <64269332+wrenfannin@users.noreply.github.com>
2022-07-04 11:34:30 +0100

    Add files via upload

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,2 +1,24 @@
-exec nitrogen --restore &
+#!/bin/sh
 
+# set wallpaper with feh
+nitrogen --restore &
+# killall if running
+killall sxhkd dwmbarscripts autostartdwmbar dunst picom flameshot 
+
+# start compositor
+picom --config ~/.dwm/picom.conf &
+
+# start sxhkd
+sxhkd &
+
+# start dwmbar config
+autostartdwmbar &
+
+# start dunst notification daemon
+dunst -config ~/.dwm/dunstrc &
+
+#start screenshot program
+flameshot &
+
+# start blueman
+blueman-applet &
