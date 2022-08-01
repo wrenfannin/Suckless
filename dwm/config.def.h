0020860ef5b6e85fd4296ff1a18f554b00deb04e
wrenfannin <luwr5102@gmail.com>
2022-08-01 16:44:14 +0100

    fixed some colours

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -21,11 +21,12 @@ static const char col_gray1[]       = "#222e3d";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#16161e";
+static const char col_border[]          = "#222e3d";
 static const char col_cyan[]        = "#8ea9c7";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border  },
 };
 
 /* tagging */
